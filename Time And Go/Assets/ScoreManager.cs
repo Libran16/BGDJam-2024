41391ebf2844183ee19cd5366861001ad0623f45
Libran16 <Libran16@users.noreply.github.com>
2024-01-07 09:28:21 +0700

    fix

--- Time And Go/Assets/ScoreManager.cs
+++ Time And Go/Assets/ScoreManager.cs
@@ -6,16 +6,32 @@ using TMPro;
 public class ScoreManager : MonoBehaviour
 {
     [SerializeField] TextMeshProUGUI scoreText;
+    [SerializeField] TextMeshProUGUI hiScoreText;
     int score;
+    int hscore;
 
     void Start()
     {
         scoreText.text = "Score : " + score;
+        if(PlayerPrefs.HasKey("HighScore"))
+        {
+            hscore = PlayerPrefs.GetInt("HighScore");
+        }
     }
 
     public void IncreaseScore()
     {
         score ++;
         scoreText.text = "Score : " + score;
+    }
+
+    public void HighScore()
+    {
+        if(score > hscore)
+        {
+            hscore = score;
+            PlayerPrefs.SetInt("HighScore", hscore);
+        }
+        hiScoreText.text = "Hi-Score : " + hscore;
     }
 }
